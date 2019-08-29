cc96a3e15827b392532c0884b3f0641bf58be50d
Hungo Hung <hungo.hung@prophetstor.com>
2019-08-29 17:47:26 +0800

    add silent mode function, check script comment for more detail
    Signed-off-by: Hungo Hung <hungo.hung@prophetstor.com>

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-##################################################################
+#################################################################################################################
 #
 #   This script is created for installing Federator.ai Operator
 #
-#   Usage: ./install.sh $tag_name or ./install.sh
-##################################################################
+#   1. Interactive Mode
+#      Usage: ./install.sh
+#
+#   2. Silent Mode - Persistent storage
+#      Usage: ./install.sh -t v4.2.260 -n federatorai -e y -p https://prometheus-k8s.openshift-monitoring:9091 \
+#                   -s persistent -l 11 -d 12 -c managed-nfs-storage
+#
+#   3. Silent Mode - Ephemeral storage
+#      Usage: ./install.sh -t v4.2.260 -n alameda -e y -p https://prometheus-k8s.openshift-monitoring:9091 \
+#                   -s ephemeral
+#
+#   -t = tag_number
+#   -n = install_namespace
+#   -e = enable_execution
+#   -p = prometheus_address
+#   -s = storage_type
+#   -l = log_size
+#   -d = data_size
+#   -c = storage_class
+#################################################################################################################
 
 is_pod_ready()
 {
@@ -71,32 +89,91 @@ get_grafana_route()
     fi
 }
 
-while [[ "$info_correct" != "y" ]] && [[ "$info_correct" != "Y" ]]
-do
-    # init variables
-    install_namespace=""
-    tag_number=""
-
-    if [ "$1" != "" ];then
-        tag_number="$1"
-    else
-        read -r -p "$(tput setaf 2)Please input Federator.ai Operator tag:$(tput sgr 0) " tag_number </dev/tty
-    fi
-
-    default="federatorai"
-    read -r -p "$(tput setaf 2)Enter the namespace you want to install Federator.ai [default: federatorai]: $(tput sgr 0)" install_namespace </dev/tty
-    install_namespace=${install_namespace:-$default}
-
-    echo -e "\n----------------------------------------"
-    echo "tag_number = $tag_number"
-    echo "install_namespace = $install_namespace"
-    echo "----------------------------------------"
-
-    default="y"
-    read -r -p "$(tput setaf 2)Is the above information correct? [default: y]: $(tput sgr 0)" info_correct </dev/tty
-    info_correct=${info_correct:-$default}
+while getopts "t:n:e:p:s:l:d:c:" o; do
+    case "${o}" in
+        t)
+            t_arg=${OPTARG}
+            ;;
+        n)
+            n_arg=${OPTARG}
+            ;;
+        e)
+            e_arg=${OPTARG}
+            ;;
+        p)
+            p_arg=${OPTARG}
+            ;;
+        s)
+            s_arg=${OPTARG}
+            ;;
+        l)
+            l_arg=${OPTARG}
+            ;;
+        d)
+            d_arg=${OPTARG}
+            ;;
+        c)
+            c_arg=${OPTARG}
+            ;;
+        *)
+            echo "Warning! wrong paramter, ignore it."
+            ;;
+    esac
 done
 
+[ "${t_arg}" = "" ] && silent_mode_disabled="y"
+[ "${n_arg}" = "" ] && silent_mode_disabled="y"
+[ "${e_arg}" = "" ] && silent_mode_disabled="y"
+[ "${p_arg}" = "" ] && silent_mode_disabled="y"
+[ "${s_arg}" = "" ] && silent_mode_disabled="y"
+[ "${s_arg}" = "persistent" ] && [ "${l_arg}" = "" ] && silent_mode_disabled="y"
+[ "${s_arg}" = "persistent" ] && [ "${d_arg}" = "" ] && silent_mode_disabled="y"
+[ "${s_arg}" = "persistent" ] && [ "${c_arg}" = "" ] && silent_mode_disabled="y"
+
+[ "${t_arg}" != "" ] && tag_number="${t_arg}"
+[ "${n_arg}" != "" ] && install_namespace="${n_arg}"
+[ "${e_arg}" != "" ] && enable_execution="${e_arg}"
+[ "${p_arg}" != "" ] && prometheus_address="${p_arg}"
+[ "${s_arg}" != "" ] && storage_type="${s_arg}"
+[ "${l_arg}" != "" ] && log_size="${l_arg}"
+[ "${d_arg}" != "" ] && data_size="${d_arg}"
+[ "${c_arg}" != "" ] && storage_class="${c_arg}"
+
+if [ "$silent_mode_disabled" = "y" ];then
+
+    while [[ "$info_correct" != "y" ]] && [[ "$info_correct" != "Y" ]]
+    do
+        # init variables
+        install_namespace=""
+        tag_number=""
+
+        read -r -p "$(tput setaf 2)Please input Federator.ai Operator tag:$(tput sgr 0) " tag_number </dev/tty
+
+        default="federatorai"
+        read -r -p "$(tput setaf 2)Enter the namespace you want to install Federator.ai [default: federatorai]: $(tput sgr 0)" install_namespace </dev/tty
+        install_namespace=${install_namespace:-$default}
+
+        echo -e "\n----------------------------------------"
+        echo "tag_number = $tag_number"
+        echo "install_namespace = $install_namespace"
+        echo "----------------------------------------"
+
+        default="y"
+        read -r -p "$(tput setaf 2)Is the above information correct? [default: y]: $(tput sgr 0)" info_correct </dev/tty
+        info_correct=${info_correct:-$default}
+    done
+else
+    echo -e "\n----------------------------------------"
+    echo "tag_number=$tag_number"
+    echo "install_namespace=$install_namespace"
+    echo "enable_execution=$enable_execution"
+    echo "prometheus_address=$prometheus_address"
+    echo "storage_type=$storage_type"
+    echo "log_size=$log_size"
+    echo "data_size=$data_size"
+    echo "storage_class=$storage_class"
+    echo -e "----------------------------------------\n"
+fi
 
 openshift_version=`oc version 2>/dev/null|grep "oc v"|cut -d '.' -f2`
 
@@ -157,84 +234,91 @@ sed -i "s/version: latest/version: ${tag_number}/g" ${alamedaservice_example}
 
 echo "========================================"
 
-while [[ "$interactive_install" != "y" ]] && [[ "$interactive_install" != "n" ]]
-do
-    default="y"
-    read -r -p "$(tput setaf 2)Do you want to launch interactive installation of Federator.ai [default: y]: $(tput sgr 0)" interactive_install </dev/tty
-    interactive_install=${interactive_install:-$default}
-done
+if [ "$silent_mode_disabled" = "y" ];then
 
-if [[ "$interactive_install" == "y" ]]; then
-
-    while [[ "$information_correct" != "y" ]] && [[ "$information_correct" != "Y" ]]
+    while [[ "$install_alameda" != "y" ]] && [[ "$install_alameda" != "n" ]]
     do
-        # init variables
-        enable_execution=""
-        prometheus_address=""
-        storage_type=""
-        log_size=""
-        data_size=""
-        storage_class=""
-
         default="y"
-        read -r -p "$(tput setaf 127)Do you want to enable execution? [default: y]: $(tput sgr 0): " enable_execution </dev/tty
-        enable_execution=${enable_execution:-$default}
-
-        if [[ "$openshift_version" == "11" ]]; then
-            default="https://prometheus-k8s.openshift-monitoring:9091"
-        elif [[ "$openshift_version" == "9" ]]; then
-            default="http://prom-prometheus-operator-prometheus.monitoring.svc:9090"
-        else
-            default="https://prometheus-k8s.openshift-monitoring:9091"
-        fi
-
-        echo "$(tput setaf 127)Enter the prometheus service address"
-        read -r -p "[default: ${default}]: $(tput sgr 0)" prometheus_address </dev/tty
-        prometheus_address=${prometheus_address:-$default}
-
-        while [[ "$storage_type" != "ephemeral" ]] && [[ "$storage_type" != "persistent" ]]
-        do
-            default="ephemeral"
-            echo "$(tput setaf 127)Which storage type you would like to use? ephemeral or persistent?"
-            read -r -p "[default: ephemeral]: $(tput sgr 0)" storage_type </dev/tty
-            storage_type=${storage_type:-$default}
-        done
-
-        if [[ "$storage_type" == "persistent" ]]; then
-            default="10"
-            read -r -p "$(tput setaf 127)Specify log storage size [ex: 10 for 10GB, default: 10]: $(tput sgr 0)" log_size </dev/tty
-            log_size=${log_size:-$default}
-            default="10"
-            read -r -p "$(tput setaf 127)Specify data storage size [ex: 10 for 10GB, default: 10]: $(tput sgr 0)" data_size </dev/tty
-            data_size=${data_size:-$default}
-
-            while [[ "$storage_class" == "" ]]
-            do
-                read -r -p "$(tput setaf 127)Specify storage class name: $(tput sgr 0)" storage_class </dev/tty
-            done
-        fi
-
-        echo -e "\n----------------------------------------"
-        echo "install_namespace = $install_namespace"
-        if [[ "$enable_execution" == "y" ]]; then
-            echo "enable_execution = true"
-        else
-            echo "enable_execution = false"
-        fi
-        echo "prometheus_address = $prometheus_address"
-        echo "storage_type = $storage_type"
-        if [[ "$storage_type" == "persistent" ]]; then
-            echo "log storage size = $log_size GB"
-            echo "data storage size = $data_size GB"
-            echo "storage class name = $storage_class"
-        fi
-        echo "----------------------------------------"
-
-        default="y"
-        read -r -p "$(tput setaf 2)Is the above information correct [default: y]:$(tput sgr 0)" information_correct </dev/tty
-        information_correct=${information_correct:-$default}
+        read -r -p "$(tput setaf 2)Do you want to launch interactive installation of Federator.ai [default: y]: $(tput sgr 0)" install_alameda </dev/tty
+        install_alameda=${install_alameda:-$default}
     done
 
+    if [[ "$install_alameda" == "y" ]]; then
+
+        while [[ "$information_correct" != "y" ]] && [[ "$information_correct" != "Y" ]]
+        do
+            # init variables
+            enable_execution=""
+            prometheus_address=""
+            storage_type=""
+            log_size=""
+            data_size=""
+            storage_class=""
+
+            default="y"
+            read -r -p "$(tput setaf 127)Do you want to enable execution? [default: y]: $(tput sgr 0): " enable_execution </dev/tty
+            enable_execution=${enable_execution:-$default}
+
+            if [[ "$openshift_version" == "11" ]]; then
+                default="https://prometheus-k8s.openshift-monitoring:9091"
+            elif [[ "$openshift_version" == "9" ]]; then
+                default="http://prom-prometheus-operator-prometheus.monitoring.svc:9090"
+            else
+                default="https://prometheus-k8s.openshift-monitoring:9091"
+            fi
+
+            echo "$(tput setaf 127)Enter the prometheus service address"
+            read -r -p "[default: ${default}]: $(tput sgr 0)" prometheus_address </dev/tty
+            prometheus_address=${prometheus_address:-$default}
+
+            while [[ "$storage_type" != "ephemeral" ]] && [[ "$storage_type" != "persistent" ]]
+            do
+                default="ephemeral"
+                echo "$(tput setaf 127)Which storage type you would like to use? ephemeral or persistent?"
+                read -r -p "[default: ephemeral]: $(tput sgr 0)" storage_type </dev/tty
+                storage_type=${storage_type:-$default}
+            done
+
+            if [[ "$storage_type" == "persistent" ]]; then
+                default="10"
+                read -r -p "$(tput setaf 127)Specify log storage size [ex: 10 for 10GB, default: 10]: $(tput sgr 0)" log_size </dev/tty
+                log_size=${log_size:-$default}
+                default="10"
+                read -r -p "$(tput setaf 127)Specify data storage size [ex: 10 for 10GB, default: 10]: $(tput sgr 0)" data_size </dev/tty
+                data_size=${data_size:-$default}
+
+                while [[ "$storage_class" == "" ]]
+                do
+                    read -r -p "$(tput setaf 127)Specify storage class name: $(tput sgr 0)" storage_class </dev/tty
+                done
+            fi
+
+            echo -e "\n----------------------------------------"
+            echo "install_namespace = $install_namespace"
+            if [[ "$enable_execution" == "y" ]]; then
+                echo "enable_execution = true"
+            else
+                echo "enable_execution = false"
+            fi
+            echo "prometheus_address = $prometheus_address"
+            echo "storage_type = $storage_type"
+            if [[ "$storage_type" == "persistent" ]]; then
+                echo "log storage size = $log_size GB"
+                echo "data storage size = $data_size GB"
+                echo "storage class name = $storage_class"
+            fi
+            echo "----------------------------------------"
+
+            default="y"
+            read -r -p "$(tput setaf 2)Is the above information correct [default: y]:$(tput sgr 0)" information_correct </dev/tty
+            information_correct=${information_correct:-$default}
+        done
+    fi
+else
+    install_alameda="y"
+fi
+
+if [[ "$install_alameda" == "y" ]]; then
     sed -i "s|\bnamespace:.*|namespace: ${install_namespace}|g" ${alamedaservice_example}
 
     if [[ "$enable_execution" == "y" ]]; then
@@ -245,8 +329,8 @@ if [[ "$interactive_install" == "y" ]]; then
 
     sed -i "s|\bprometheusService:.*|prometheusService: ${prometheus_address}|g" ${alamedaservice_example}
     if [[ "$storage_type" == "persistent" ]]; then
-            sed -i '/- usage:/,+10d' ${alamedaservice_example}
-            cat >> ${alamedaservice_example} << __EOF__
+        sed -i '/- usage:/,+10d' ${alamedaservice_example}
+        cat >> ${alamedaservice_example} << __EOF__
     - usage: log
       type: pvc
       size: ${log_size}Gi
