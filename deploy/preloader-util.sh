d3f41c35679a60e37fdb9a34839e2d9a8f85bfa2
Hungo Hung <hungo.hung@prophetstor.com>
2020-04-07 21:56:20 +0800

    change cpu limit back to 150m
    Signed-off-by: Hungo Hung <hungo.hung@prophetstor.com>

--- deploy/preloader-util.sh
+++ deploy/preloader-util.sh
@@ -545,7 +545,7 @@ spec:
         - name: ${nginx_dc_name}
           resources:
             limits:
-                cpu: "100m"
+                cpu: "150m"
                 memory: "400Mi"
             requests:
                 cpu: "100m"
@@ -587,7 +587,7 @@ spec:
         image: nginx:1.7.9
         resources:
             limits:
-                cpu: "100m"
+                cpu: "150m"
                 memory: "400Mi"
             requests:
                 memory: "100Mi"
