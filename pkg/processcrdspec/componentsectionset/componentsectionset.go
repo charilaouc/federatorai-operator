415dc8a52149974f5230986669f0edbc3121e4c5
Frederic Huang <kuofu.huang@prophetstor.com>
2020-06-30 12:26:37 +0800

    add resources configuration in alamedaservice
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>
    (cherry picked from commit 6f52d0c390024d52146f04be38c0a0d5595e3af5)
    (cherry picked from commit 15e214d8c24990943879d866f6b2b87d589f9b4e)

--- pkg/processcrdspec/componentsectionset/componentsectionset.go
+++ pkg/processcrdspec/componentsectionset/componentsectionset.go
@@ -14,119 +14,144 @@ import (
 func SectionSetParamterToDeployment(dep *appsv1.Deployment, asp *alamedaserviceparamter.AlamedaServiceParamter) {
 
 	envVars := []corev1.EnvVar{}
+	resources := corev1.ResourceRequirements{}
 	switch dep.Name {
 	case util.AlamedaaiDPN:
 		envVars = asp.AlamedaAISectionSet.EnvVars
+		resources = asp.AlamedaAISectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaaiCTN, asp.AlamedaAISectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaAISectionSet.Storages, "alameda-ai-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaAISectionSet.Storages, util.AlamedaaiCTN, "alameda-ai-type-storage", util.AlamedaGroup)
 	case util.AlamedaoperatorDPN:
 		envVars = asp.AlamedaOperatorSectionSet.EnvVars
+		resources = asp.AlamedaOperatorSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaoperatorCTN, asp.AlamedaOperatorSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaOperatorSectionSet.Storages, "alameda-operator-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaOperatorSectionSet.Storages, util.AlamedaoperatorCTN, "alameda-operator-type-storage", util.AlamedaGroup)
 	case util.AlamedadatahubDPN:
 		envVars = asp.AlamedaDatahubSectionSet.EnvVars
+		resources = asp.AlamedaDatahubSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedadatahubCTN, asp.AlamedaDatahubSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaDatahubSectionSet.Storages, "alameda-datahub-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaDatahubSectionSet.Storages, util.AlamedadatahubCTN, "alameda-datahub-type-storage", util.AlamedaGroup)
 	case util.AlamedaevictionerDPN:
 		envVars = asp.AlamedaEvictionerSectionSet.EnvVars
+		resources = asp.AlamedaEvictionerSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaevictionerCTN, asp.AlamedaEvictionerSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaEvictionerSectionSet.Storages, "alameda-evictioner-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaEvictionerSectionSet.Storages, util.AlamedaevictionerCTN, "alameda-evictioner-type-storage", util.AlamedaGroup)
 	case util.AdmissioncontrollerDPN:
 		envVars = asp.AdmissionControllerSectionSet.EnvVars
+		resources = asp.AdmissionControllerSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AdmissioncontrollerCTN, asp.AdmissionControllerSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AdmissionControllerSectionSet.Storages, "admission-controller-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AdmissionControllerSectionSet.Storages, util.AdmissioncontrollerCTN, "admission-controller-type-storage", util.AlamedaGroup)
 	case util.AlamedarecommenderDPN:
 		envVars = asp.AlamedaRecommenderSectionSet.EnvVars
+		resources = asp.AlamedaRecommenderSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedarecommenderCTN, asp.AlamedaRecommenderSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaRecommenderSectionSet.Storages, "alameda-recommender-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaRecommenderSectionSet.Storages, util.AlamedarecommenderCTN, "alameda-recommender-type-storage", util.AlamedaGroup)
 	case util.AlamedaexecutorDPN:
 		envVars = asp.AlamedaExecutorSectionSet.EnvVars
+		resources = asp.AlamedaExecutorSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaexecutorCTN, asp.AlamedaExecutorSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaExecutorSectionSet.Storages, "alameda-executor-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaExecutorSectionSet.Storages, util.AlamedaexecutorCTN, "alameda-executor-type-storage", util.AlamedaGroup)
 	case util.AlamedadispatcherDPN:
 		envVars = asp.AlamedaDispatcherSectionSet.EnvVars
+		resources = asp.AlamedaDispatcherSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedadispatcherCTN, asp.AlamedaDispatcherSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaDispatcherSectionSet.Storages, "alameda-dispatcher-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaDispatcherSectionSet.Storages, util.AlamedadispatcherCTN, "alameda-dispatcher-type-storage", util.AlamedaGroup)
 	case util.AlamedaRabbitMQDPN:
 		envVars = asp.AlamedaRabbitMQSectionSet.EnvVars
+		resources = asp.AlamedaRabbitMQSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaRabbitMQCTN, asp.AlamedaRabbitMQSectionSet.ImagePullPolicy)
 	case util.AlamedaanalyzerDPN:
 		envVars = asp.AlamedaAnalyzerSectionSet.EnvVars
+		resources = asp.AlamedaAnalyzerSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaanalyzerCTN, asp.AlamedaAnalyzerSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaAnalyzerSectionSet.Storages, "alameda-analyzer-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaAnalyzerSectionSet.Storages, util.AlamedaanalyzerCTN, "alameda-analyzer-type-storage", util.AlamedaGroup)
 	case util.FedemeterDPN:
 		envVars = asp.AlamedaFedemeterSectionSet.EnvVars
+		resources = asp.AlamedaFedemeterSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FedemeterCTN, asp.AlamedaFedemeterSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaFedemeterSectionSet.Storages, "fedemeter-type.pvc", util.FedemeterGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaFedemeterSectionSet.Storages, util.FedemeterCTN, "fedemeter-type-storage", util.FedemeterGroup)
 	case util.InfluxdbDPN:
 		envVars = asp.InfluxdbSectionSet.EnvVars
+		resources = asp.InfluxdbSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.InfluxdbCTN, asp.InfluxdbSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.InfluxdbSectionSet.Storages, "my-alameda.influxdb-type.pvc", util.InfluxDBGroup)
 		util.SetStorageToMountPath(dep, asp.InfluxdbSectionSet.Storages, util.InfluxdbCTN, "influxdb-type-storage", util.InfluxDBGroup)
 	case util.GrafanaDPN:
 		envVars = asp.GrafanaSectionSet.EnvVars
+		resources = asp.GrafanaSectionSet.Resources
 		util.SetBootStrapImageStruct(dep, asp.GrafanaSectionSet, util.GetTokenCTN)
 		util.SetImagePullPolicy(dep, util.GrafanaCTN, asp.GrafanaSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.GrafanaSectionSet.Storages, "my-alameda.grafana-type.pvc", util.GrafanaGroup)
 		util.SetStorageToMountPath(dep, asp.GrafanaSectionSet.Storages, util.GrafanaCTN, "grafana-type-storage", util.GrafanaGroup)
 	case util.AlamedaweavescopeDPN:
 		envVars = asp.AlamedaWeavescopeSectionSet.EnvVars
+		resources = asp.AlamedaWeavescopeSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaweavescopeCTN, asp.AlamedaWeavescopeSectionSet.ImagePullPolicy)
 	case util.AlamedaweavescopeProbeDPN:
 		envVars = asp.AlamedaWeavescopeSectionSet.EnvVars
+		resources = asp.AlamedaWeavescopeSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaweavescopeProbeCTN, asp.AlamedaWeavescopeSectionSet.ImagePullPolicy)
 	case util.AlamedaNotifierDPN:
 		envVars = asp.AlamedaNotifierSectionSet.EnvVars
+		resources = asp.AlamedaNotifierSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.AlamedaNofitierCTN, asp.AlamedaNotifierSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.AlamedaNotifierSectionSet.Storages, "alameda-notifier-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.AlamedaNotifierSectionSet.Storages, util.AlamedaNofitierCTN, "alameda-notifier-type-storage", util.AlamedaGroup)
 	case util.FederatoraiAgentDPN:
 		envVars = asp.FederatoraiAgentSectionSet.EnvVars
+		resources = asp.FederatoraiAgentSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiAgentCTN, asp.FederatoraiAgentSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiAgentSectionSet.Storages, "federatorai-agent-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiAgentSectionSet.Storages, util.FederatoraiAgentCTN, "federatorai-agent-type-storage", util.AlamedaGroup)
 	case util.FederatoraiAgentGPUDPN:
 		envVars = asp.FederatoraiAgentGPUSectionSet.EnvVars
+		resources = asp.FederatoraiAgentGPUSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiAgentGPUCTN, asp.FederatoraiAgentGPUSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiAgentGPUSectionSet.Storages, "federatorai-agent-gpu-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiAgentGPUSectionSet.Storages, util.FederatoraiAgentGPUCTN, "federatorai-agent-gpu-type-storage", util.AlamedaGroup)
 	case util.FederatoraiAgentAppDPN:
 		envVars = asp.FederatoraiAgentAppSectionSet.EnvVars
+		resources = asp.FederatoraiAgentAppSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiAgentAppCTN, asp.FederatoraiAgentAppSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiAgentAppSectionSet.Storages, "federatorai-agent-app-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiAgentAppSectionSet.Storages, util.FederatoraiAgentAppCTN, "federatorai-agent-app-type-storage", util.AlamedaGroup)
 	case util.FederatoraiRestDPN:
 		envVars = asp.FederatoraiRestSectionSet.EnvVars
+		resources = asp.FederatoraiRestSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiRestCTN, asp.FederatoraiRestSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiRestSectionSet.Storages, "federatorai-rest-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiRestSectionSet.Storages, util.FederatoraiRestCTN, "federatorai-rest-type-storage", util.AlamedaGroup)
 	case util.FederatoraiAgentPreloaderDPN:
 		envVars = asp.FederatoraiAgentPreloaderSectionSet.EnvVars
+		resources = asp.FederatoraiAgentPreloaderSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiAgentPreloaderCTN, asp.FederatoraiAgentPreloaderSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiAgentPreloaderSectionSet.Storages, "federatorai-agent-preloader-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiAgentPreloaderSectionSet.Storages, util.FederatoraiAgentPreloaderCTN, "federatorai-agent-preloader-type-storage", util.AlamedaGroup)
 	case util.FederatoraiFrontendDPN:
 		envVars = asp.FederatoraiFrontendSectionSet.EnvVars
+		resources = asp.FederatoraiFrontendSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiFrontendCTN, asp.FederatoraiFrontendSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiFrontendSectionSet.Storages, "federatorai-frontend-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiFrontendSectionSet.Storages, util.FederatoraiFrontendCTN, "federatorai-frontend-type-storage", util.AlamedaGroup)
 	case util.FederatoraiBackendDPN:
 		envVars = asp.FederatoraiBackendSectionSet.EnvVars
+		resources = asp.FederatoraiBackendSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiBackendCTN, asp.FederatoraiBackendSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiBackendSectionSet.Storages, "federatorai-backend-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiBackendSectionSet.Storages, util.FederatoraiBackendCTN, "federatorai-backend-type-storage", util.AlamedaGroup)
 	case util.FederatoraiDataAdapterDPN:
 		envVars = asp.FederatoraiDataAdapterSectionSet.EnvVars
+		resources = asp.FederatoraiDataAdapterSectionSet.Resources
 		util.SetImagePullPolicy(dep, util.FederatoraiDataAdapterCTN, asp.FederatoraiDataAdapterSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(dep, asp.FederatoraiDataAdapterSectionSet.Storages, "federatorai-data-adapter-type.pvc", util.AlamedaGroup)
 		util.SetStorageToMountPath(dep, asp.FederatoraiDataAdapterSectionSet.Storages, util.FederatoraiDataAdapterCTN, "federatorai-data-adapter-type-storage", util.AlamedaGroup)
@@ -136,13 +161,17 @@ func SectionSetParamterToDeployment(dep *appsv1.Deployment, asp *alamedaservicep
 		newEnv := replaceOrAppendEnvVar(container.Env, envVars)
 		dep.Spec.Template.Spec.Containers[i].Env = newEnv
 	}
+	util.SetResourcesForContainers(dep, resources, false)
+	util.SetResourcesForContainers(dep, resources, true)
 }
 
 func SectionSetParamterToDaemonSet(ds *appsv1.DaemonSet, asp *alamedaserviceparamter.AlamedaServiceParamter) {
 	envVars := []corev1.EnvVar{}
+	resources := corev1.ResourceRequirements{}
 	switch ds.Name {
 	case util.AlamedaweavescopeAgentDS:
 		envVars = asp.AlamedaWeavescopeSectionSet.EnvVars
+		resources = asp.AlamedaWeavescopeSectionSet.Resources
 		util.SetDaemonSetImagePullPolicy(ds, util.AlamedaweavescopeAgentCTN, asp.AlamedaWeavescopeSectionSet.ImagePullPolicy)
 	}
 
@@ -150,13 +179,18 @@ func SectionSetParamterToDaemonSet(ds *appsv1.DaemonSet, asp *alamedaservicepara
 		newEnv := replaceOrAppendEnvVar(container.Env, envVars)
 		ds.Spec.Template.Spec.Containers[i].Env = newEnv
 	}
+
+	util.SetResourcesForContainers(ds, resources, false)
+	util.SetResourcesForContainers(ds, resources, true)
 }
 
 func SectionSetParamterToStatefulSet(ss *appsv1.StatefulSet, asp *alamedaserviceparamter.AlamedaServiceParamter) {
 	envVars := []corev1.EnvVar{}
+	resources := corev1.ResourceRequirements{}
 	switch ss.Name {
 	case util.FedemeterInflixDBSSN:
 		envVars = asp.AlamedaFedemeterInfluxdbSectionSet.EnvVars
+		resources = asp.AlamedaFedemeterInfluxdbSectionSet.Resources
 		util.SetStatefulSetImagePullPolicy(ss, util.FedemeterInfluxDBCTN, asp.InfluxdbSectionSet.ImagePullPolicy)
 	}
 
@@ -164,6 +198,8 @@ func SectionSetParamterToStatefulSet(ss *appsv1.StatefulSet, asp *alamedaservice
 		newEnv := replaceOrAppendEnvVar(container.Env, envVars)
 		ss.Spec.Template.Spec.Containers[i].Env = newEnv
 	}
+	util.SetResourcesForContainers(ss, resources, false)
+	util.SetResourcesForContainers(ss, resources, true)
 }
 
 func SectionSetParamterToPersistentVolumeClaim(pvc *corev1.PersistentVolumeClaim, asp *alamedaserviceparamter.AlamedaServiceParamter) {
