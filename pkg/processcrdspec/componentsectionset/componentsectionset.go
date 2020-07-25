4cb32bd231975b8213b4cad2c07d64b19e378890
Frederic Huang <kuofu.huang@prophetstor.com>
2020-07-25 15:41:41 +0800

    fix resource setting not applied in alameda-influxdb
    Signed-off-by: Frederic Huang <kuofu.huang@prophetstor.com>
    (cherry picked from commit 372788aee5a7fca06b6c63d2ff5522252897f2ef)

--- pkg/processcrdspec/componentsectionset/componentsectionset.go
+++ pkg/processcrdspec/componentsectionset/componentsectionset.go
@@ -187,6 +187,7 @@ func SectionSetParamterToStatefulSet(ss *appsv1.StatefulSet, asp *alamedaservice
 			"fedemeter-influxdb-type-storage", util.InfluxDBGroup)
 	case util.InfluxdbDPN:
 		envVars = asp.InfluxdbSectionSet.EnvVars
+		resources = asp.InfluxdbSectionSet.Resources
 		util.SetImagePullPolicy(ss, util.InfluxdbCTN, asp.InfluxdbSectionSet.ImagePullPolicy)
 		util.SetStorageToVolumeSource(ss, asp.InfluxdbSectionSet.Storages,
 			"my-alameda.influxdb-type.pvc", util.InfluxDBGroup)
